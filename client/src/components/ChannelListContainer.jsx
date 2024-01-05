bc481f6728a7c6d7868ba61bb27af05ef765dda8
AliHamdi63 <aawere1233@gmail.com>
2024-01-05 18:37:51 +0200

    commit

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,17 +3,17 @@ import { ChannelList, useChatContext } from "stream-chat-react"
 import Cookies from "universal-cookie";
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./"
-import ChatIcon from "../assets/hospital.png"
+// import ChatIcon from "../assets/hospital.png"
 import LogoutIcon from "../assets/logout.png"
 
 const cookies = new Cookies();
 const SideBar = ({ logout }) => (
     <div className='channel-list__sidebar'>
-        <div className='channel-list__sidebar__icon1'>
+        {/* <div className='channel-list__sidebar__icon1'>
             <div className='icon1__inner'>
                 <img src={ChatIcon} alt='Chat' width='30' />
             </div>
-        </div>
+        </div> */}
         <div className='channel-list__sidebar__icon2'>
             <div className='icon1__inner' onClick={logout}>
                 <img src={LogoutIcon} alt='Logout' width='30' />
@@ -58,7 +58,7 @@ const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEdi
             <SideBar logout={logout} />
             <div className='channel-list__list__wrapper'>
                 <CompanyHeader />
-                <ChannelSearch />
+                <ChannelSearch setToggleContainer={setToggleContainer} />
                 <ChannelList
                     filters={filters}
                     channelRenderFilterFn={customChannelTeamFilter}
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,6 +3,7 @@ export { default as ChannelContainer } from "./ChannelContainer";
 export { default as ChannelSearch } from "./channel-list/ChannelSearch";
 export { default as TeamChannelList } from "./channel-list/TeamChannelList";
 export { default as TeamChannelPreview } from "./channel-list/TeamChannelPreview";
+export { default as ResultsDropdown } from "./channel-list/ResultsDropdown";
 export { default as Auth } from "./auth/Auth";
 export { default as ChannelInner } from "./channel-container/ChannelInner";
 export { default as CreateChannel } from "./channel-container/CreateChannel";
