33429684562d7222a5a7ac5d2ca5ae407b37d6df
AliHamdi63 <aawere1233@gmail.com>
2024-01-05 19:13:31 +0200

    commit

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/auth", require('./routes/auth'));
 
-
+app.get('/', (req, res) => {
+    res.send('Hello!')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
